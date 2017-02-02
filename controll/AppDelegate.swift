17cf8b71cb1f7a6a47497d4804451efec3aee5a6
sviceman <sviceman@yandex.com>
2017-02-02 14:58:31 -0500

    Initial Commit

---
+++ controll/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  controll
+//
+//  Created by Serghey Vice on 2/2/17.
+//  Copyright © 2017 Purple i2p. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ controll/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  controll
+//
+//  Created by Serghey Vice on 2/2/17.
+//  Copyright © 2017 Purple i2p. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
